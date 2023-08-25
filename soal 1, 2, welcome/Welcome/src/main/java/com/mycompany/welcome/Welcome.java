d46763a7fdacabfc43e0bc89e5dac4b453b6fba9
gianvilcanpatra <123915139+gianvilcanpatra@users.noreply.github.com>
2023-08-25 23:09:46 +0700

    Add files via upload

---
+++ soal 1, 2, welcome/Welcome/src/main/java/com/mycompany/welcome/Welcome.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.welcome;
+
+/**
+ * 
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++)
+        System.out.print("=");
+        System.out.println();
+    }
+}
