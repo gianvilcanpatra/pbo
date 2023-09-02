13d5f75a168893063ad65fe76be5a6ea87b37001
gianvilcanpatra <123915139+gianvilcanpatra@users.noreply.github.com>
2023-09-02 22:40:53 +0700

    Add files via upload
    minggu ke-2

---
+++ Minggu 2/soal3/src/main/java/com/mycompany/soal3/Soal3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal3;
+
+/**
+ *
+ * @author Gian Vilcan Patra
+ */
+Class FloatingPoint{
+ public static void main(String[] args) {
+ double x = 92.98;
+ int nx = (int) Math.round(x);
+ }
+}
+
